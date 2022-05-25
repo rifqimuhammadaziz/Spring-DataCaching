94c499b11d0045bd586320939ea8091f00d6c2cb
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-05-25 08:52:13 +0700

    Create spring project

---
+++ src/main/java/rifqimuhammadaziz/springdatacaching/SpringDataCachingApplication.java
@@ -0,0 +1,13 @@
+package rifqimuhammadaziz.springdatacaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataCachingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataCachingApplication.class, args);
+    }
+
+}
